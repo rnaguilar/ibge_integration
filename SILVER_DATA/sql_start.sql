4d129be6dfbb9538f82cf8e1adb351333795d757
Rodrigo <rodrigo.aguilar@s3bank.com>
2024-02-03 10:34:34 -0300

    Adicionado Bando de Dados (Camada Silver)

--- SILVER_DATA/sql_start.sql
+++ SILVER_DATA/sql_start.sql
@@ -1,14 +1,3 @@
--- Criar tabela public.estado
-CREATE TABLE public.estado (
-    id_estado  PRIMARY KEY,
-    uf CHAR(2) NOT NULL,
-    estado VARCHAR(255) NOT NULL,
-    latitude VARCHAR(20) NOT NULL,
-    longitude VARCHAR(20) NOT NULL,
-    id_regiao INT NOT NULL,
-    FOREIGN KEY (id_regiao) REFERENCES public.regiao(id_regiao)
-);
-
 -- Criar tabela public.regiao
 CREATE TABLE public.regiao (
     id_regiao INT PRIMARY KEY,
@@ -21,20 +10,27 @@ CREATE TABLE public.tipo_pesquisa (
     pesquisa VARCHAR(255) NOT NULL
 );
 
+-- Criar tabela public.estado
+CREATE TABLE public.estado (
+    id_estado  INT PRIMARY KEY,
+    uf CHAR(2) NOT NULL,
+    estado VARCHAR(255) NOT NULL,
+    latitude VARCHAR(20) NOT NULL,
+    longitude VARCHAR(20) NOT NULL,
+    id_regiao INT NOT NULL,
+    FOREIGN KEY (id_regiao) REFERENCES public.regiao(id_regiao)
+);
+
+
 -- Criar tabela public.pesquisas
 CREATE TABLE public.pesquisas (
     id_registro SERIAL PRIMARY KEY,
     ano INT NOT NULL,
     resultado NUMERIC NOT NULL,
-    cd_pesquisa INT NOT NULL,
+    cd_pesquisa VARCHAR NOT NULL,
     id_sexo INT ,
     id_faixa INT,
     id_estado INT NOT NULL,
     FOREIGN KEY (cd_pesquisa) REFERENCES public.tipo_pesquisa(cd_pesquisa),
     FOREIGN KEY (id_estado) REFERENCES public.estado(id_estado)
 );
-
--- Adicionar coluna id_regiao à tabela public.pesquisas
-ALTER TABLE public.pesquisas
-ADD COLUMN id_regiao INT NOT NULL,
-ADD CONSTRAINT fk_pesquisas_regiao FOREIGN KEY (id_regiao) REFERENCES public.regiao(id_regiao);
